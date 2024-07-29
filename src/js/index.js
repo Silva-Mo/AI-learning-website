5ce412acc8025eff2099b7670700385d5deebebe
Silva-Mo <thediamondheart890@gmail.com>
2024-07-29 18:30:14 +0400

    Enhance UI compatibility with mobile phons, fix JS

--- src/js/index.js
+++ src/js/index.js
@@ -1,9 +1,3 @@
 import '../css/normalize.css';
 import '../css/styles.css';
 import * as scroll from './scroll.js'
-
-const discoverBtn = document.querySelector('.discover');
-
-discoverBtn.addEventListener('click', () => {
-    
-})
